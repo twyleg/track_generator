21f6072a8bf52ba15eb8f43eccbabc71f3ea0263
Torsten Wylegala <mail@twyleg.de>
2022-10-06 21:04:34 +0200

    RELEASE of version

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 VERSION=$1
 
 echo $VERSION
@@ -11,4 +13,24 @@ fi
 
 if [[ `git status -s | wc -l` -ne 0 ]]; then
 	echo ERROR: Git repository has uncommited changes. Please commit \& push them or stash them before preceeding.
+	# exit -2
 fi
+
+if [[ `git branch --show-current` != "master" ]]; then
+	echo ERROR: Current branch is not master, please checkout master
+	exit -3
+fi
+
+echo "Bumping VERSION.txt to $VERSION"
+echo $VERSION > $SCRIPT_DIR/../VERSION.txt
+
+echo "Creating RELEASE commit for version $VERSION"
+git add ..
+git commit -m "RELEASE of version $VERRSION"
+
+VERSION_TAG=v$VERSION
+echo "Creating tag \"$VERSION_TAG\""
+git tag $VERSION_TAG
+
+echo "Pushing master branch and tag to origin"
+# git push origin master $VERSION_TAG

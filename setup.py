40631d781f6688b1e53dfb8294efa34dcb25dde4
Torsten Wylegala <mail@twyleg.de>
2022-10-19 01:17:12 +0200

    Fixed wrong reference for enry point

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'track_generator = track_generator.starter:run',
+            'track_generator = track_generator.starter:start',
         ]
     }
 )
